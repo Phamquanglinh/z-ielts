386127712380d924860a0ffc57ef08edda1945c2
Phamquanglinh <Linh2002gv@gmail.com>
2021-05-12 15:36:07 +0700

    :

--- config/mail.php
+++ config/mail.php
@@ -43,6 +43,7 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
+            'from' => array('address' => 'bizenglishvietnam@gmail.com', 'name' => 'Admin'),
         ],
 
         'ses' => [
